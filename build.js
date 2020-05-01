58b71a58557ab884b8add4fff39d29432c2e60a4
ardittristan <ardittristan@gmail.com>
2020-05-01 18:44:11 +0200

    add back zipping to build for smaller download

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const { exec } = require('pkg');
 const { join } = require('path');
 const resourceHacker = require('@lorki97/node-resourcehacker');
 const os = require('os');
+const archive = require('simple-archiver').archive;
 const { existsSync, mkdirSync } = require('fs');
 const cachePath = join(os.homedir(), '.pkg-cache/v2.6/fetched-v13.12.0-win-x64');
 const icon = join(__dirname, 'iconset.ico');
@@ -24,10 +25,12 @@ async function main() {
             resourceType: 'ICONGROUP',
             resourceName: '1'
         }, async function () {
-            generate();
+            await generate();
+            zipFiles();
         });
     } else {
-        generate();
+        await generate();
+        zipFiles();
     }
 }
 
@@ -36,5 +39,12 @@ async function main() {
 
 async function generate() {
     await exec(['.', '--targets', 'node13.12.0-win-x64,node13.12.0-macos-x64,node13.12.0-linux-x64', '--out-path', './build']);
+
     return;
+}
+
+async function zipFiles() {
+    archive([{ data: './build/dicelog-win.exe', type: 'file', name: 'dicelog.exe' }], { output: './build/Windows.zip' });
+    archive([{ data: './build/dicelog-linux', type: 'file', name: 'dicelog' }], { output: './build/Linux.zip' });
+    archive([{ data: './build/dicelog-macos', type: 'file', name: 'dicelog' }], { output: './build/MacOS.zip' });
 }
